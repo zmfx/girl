f11b620f11a04ccb77b242082bf662f201969cfd
zmfx <zmfx_242781365@outlook.com>
2018-04-15 00:16:01 +0800

    SpringBoot-Init

---
+++ src/main/java/com/zmfx/girl/GirlProperties.java
@@ -0,0 +1,35 @@
+package com.zmfx.girl;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "girl")
+public class GirlProperties {
+    private String cupSize;
+    private Integer age;
+
+    public String getCupSize() {
+        return cupSize;
+    }
+
+    public void setCupSize(String cupSize) {
+        this.cupSize = cupSize;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "GirlProperties{" +
+                "cupSize='" + cupSize + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
---
+++ src/main/java/com/zmfx/girl/HelloController.java
@@ -0,0 +1,29 @@
+package com.zmfx.girl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @Value("${cupSize}")
+    private String cupSize;
+    @Value("${age}")
+    private int age;
+
+    @Value("${content}")
+    private String content;
+
+    @Autowired
+    private GirlProperties girlProperties;
+
+    @RequestMapping(value = "/hello",method = RequestMethod.GET)
+    public String say(){
+        //return "Hello Spring Boot!";
+        //return cupSize+age;
+        //return content;
+        return girlProperties.toString();
+    }
+}
